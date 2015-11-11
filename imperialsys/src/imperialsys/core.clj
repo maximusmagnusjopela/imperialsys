49d03c6699f7d7e84145f0ff0ae456e2e4107a5c
Jonathan Pelletier <maximusmagnusjopela@gmail.com>
2015-11-10 19:16:23 -0500

    Adding port cl arg parsing

--- imperialsys/src/imperialsys/core.clj
+++ imperialsys/src/imperialsys/core.clj
@@ -1,41 +1,16 @@
 (ns imperialsys.core
   (:gen-class)
-  (:require [hiccup.core :refer :all]))
+  (:require [clojure.tools.cli :refer [parse-opts]]))
+
+(def cli-opts
+  [["-p" "--port PORT" "Port number"
+    :default 80
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]])
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Imperial Information Systems Corporation iwebsite"
   [& args]
-  (println "Hello, World!"))
-
-(defn base
-  [title & content]
-  [:html
-   [:head
-    [:title title]]
-   [:body content]])
-
-(defn nav-bar
-  [items]
-  [:ul (for [i items] [:li i])])
-
-(defn article
-  [{:keys [title author txt]}]
-  [:div {:class "article"} 
-   [:h1 title]
-   [:p txt]
-   [:p author]])
-
-(defn footer
-  [items]
-  [:footer
-   [:ul
-    (for [i items] [:li i])]])
-
-(base "hello world" 
-      (nav-bar ["nav1" "nav2"])
-      (article {:title "an oath of faith"
-                :txt "I am taking my oath of faith with clojure !"
-                :author "Jonathan Pelletier the great"})
-      (footer ["hello1" "hello2" "hello3"]))
-      
+  (let [{:keys [options arguments summary errors]} (parse-opts args cli-opts)]
+    (println options)))
 
